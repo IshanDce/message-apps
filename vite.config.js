bd028eeef25df0111bb3adf7707cb2cc3d9e09ca
Ishan Sharma <sharmaishan74481@gmail.com>
2025-09-24 14:54:31 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+   
   plugins: [tailwindcss(),react()],
   server: {
     port: 5002,
   },
+  base: '/message-app/',
 })
